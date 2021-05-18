e75ff51457e2d571b49f7edbe01d5bc6d84b6f7c
HGandjurov <gandjurov@abv.bg>
2021-05-18 16:47:04 +0300

    Saving Related Data

--- BookStoresWebAPI/BookStoresWebAPI/Controllers/PublishersController.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Controllers/PublishersController.cs
@@ -41,15 +41,59 @@ namespace BookStoresWebAPI.Controllers
         [HttpGet("GetPublisherDetails/{id}")]
         public ActionResult<Publisher> GetPublisherDetails(int id)
         {
-            var publisher = dbContext.Publishers
+            var publishers = dbContext.Publishers
                                      .Include(pub => pub.Books)
                                         .ThenInclude(book => book.Sales)
                                      .Include(pub => pub.Users)
                                      .Where(pub => pub.PubId == id)
                                      .FirstOrDefault();
 
-            if (publisher == null)
+            if (publishers == null)
                 return NotFound();
+
+            return publishers;
+        }
+
+        // Post: api/Publishers/5
+        [HttpGet("PostPublisherDetails")]
+        public ActionResult<Publisher> PostPublisherDetails()
+        {
+            var publisher = new Publisher();
+            publisher.PublisherName = "Harper & Brothers";
+            publisher.City = "New York City";
+            publisher.State = "NY";
+            publisher.Country = "USA";
+
+            Book book1 = new Book();
+            book1.Title = "Good Night moon - 1";
+            book1.PublishedDate = DateTime.Now;
+
+            Book book2 = new Book();
+            book2.Title = "Good Night moon - 2";
+            book2.PublishedDate = DateTime.Now;
+
+            Sale sale1 = new Sale();
+            sale1.Quantity = 2;
+            sale1.StoreId = "8042";
+            sale1.OrderNum = "XYZ";
+            sale1.PayTerms = "Net 30";
+            sale1.OrderDate = DateTime.Now;
+
+            Sale sale2 = new Sale();
+            sale2.Quantity = 2;
+            sale2.StoreId = "7131";
+            sale2.OrderNum = "QA879.1";
+            sale2.PayTerms = "Net 30";
+            sale2.OrderDate = DateTime.Now;
+
+            book1.Sales.Add(sale1);
+            book2.Sales.Add(sale2);
+
+            publisher.Books.Add(book1);
+            publisher.Books.Add(book2);
+
+            dbContext.Publishers.Add(publisher);
+            dbContext.SaveChanges();
 
             return publisher;
         }
