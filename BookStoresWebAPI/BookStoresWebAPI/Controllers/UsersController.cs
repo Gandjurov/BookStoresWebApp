a01aa0f1d878a6fd91333c3eb49da7e604f28c59
HGandjurov <gandjurov@abv.bg>
2021-05-19 11:47:41 +0300

    Add JWT Authorization

--- BookStoresWebAPI/BookStoresWebAPI/Controllers/UsersController.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Controllers/UsersController.cs
@@ -2,24 +2,31 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BookStoresWebAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class UsersController : ApiController
     {
 
         private readonly BookStoresDBContext dbContext;
+        private readonly JWTSettings jwtSettings;
 
-        public UsersController(BookStoresDBContext context)
+        public UsersController(BookStoresDBContext context, IOptions<JWTSettings> jwtSettings)
         {
             dbContext = context;
+            this.jwtSettings = jwtSettings.Value;
         }
 
         // GET: api/<UsersController>
@@ -39,6 +46,35 @@ namespace BookStoresWebAPI.Controllers
                 return NotFound();
 
             return user;
+        }
+
+        [HttpGet("Login")]
+        public async Task<ActionResult<UserWithToken>> Login([FromBody] User user)
+        {
+            user = await dbContext.Users.Where(u => u.EmailAddress == user.EmailAddress
+                                                && u.Password == user.Password).FirstOrDefaultAsync();
+
+            UserWithToken userWithToken = new UserWithToken(user);
+
+            if (userWithToken == null)
+                return NotFound();
+
+            // sign your token here...
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jwtSettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.EmailAddress)
+                }),
+                Expires = DateTime.UtcNow.AddMonths(6),
+                SigningCredentials = new SigningCredentials( new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            userWithToken.Token = tokenHandler.WriteToken(token);
+
+            return userWithToken;
         }
 
         [HttpGet("GetUser")]
--- BookStoresWebAPI/BookStoresWebAPI/Startup.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Startup.cs
@@ -1,6 +1,7 @@
 using BookStoresWebAPI.Handlers;
 using BookStoresWebAPI.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,11 +11,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BookStoresWebAPI
@@ -39,10 +42,37 @@ namespace BookStoresWebAPI
                     .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
 
             // add Basic Authentication
-            services.AddAuthentication("BasicAuthentication")
-                    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+            //services.AddAuthentication("BasicAuthentication")
+            //        .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 
             services.AddDbContext<BookStoresDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // JWT Settings
+            var jwtSection = Configuration.GetSection("JWTSettings");
+            services.Configure<JWTSettings>(jwtSection);
+
+            //to validate the token which has been sent by clients
+            var appSettings = jwtSection.Get<JWTSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.SecretKey);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
